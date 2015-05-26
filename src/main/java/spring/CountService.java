4c4b2a5332ed10bc66b5e6e4d4e771db67347af3
dbusel <dmitry.busel@landsend.com>
2015-05-26 14:51:02 +0300

    changes

--- src/main/java/spring/CountService.java
+++ src/main/java/spring/CountService.java
@@ -1,49 +1,60 @@
 package spring;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.springframework.stereotype.Service;
+
+
 @Service
-public class CountService {
+public class CountService
+{
 
-    String folderPath;
-    String storageType;
+	private String folderPath;
+	private String storageType;
+	private CountersFactory countersFactory;
 
-    @Autowired
-    public CountersFactory countersFactory;
+	public String getStorageType()
+	{
+		return storageType;
+	}
 
-    public String getStorageType() {
-        return storageType;
-    }
-
-    public void setStorageType(String storageType) {
-        this.storageType = storageType;
-    }
-
+	public void setStorageType(String storageType)
+	{
+		this.storageType = storageType;
+	}
 
 
-    public String getFolderPath() {
-        return folderPath;
-    }
 
-    public void setFolderPath(String folderPath) {
-        this.folderPath = folderPath;
-    }
+	public String getFolderPath()
+	{
+		return folderPath;
+	}
 
-    ArrayList<WordsContainer> start() throws IOException {
-        File folder = new File(this.folderPath);
-        ArrayList<WordsContainer> wordsContainers = new ArrayList<>();
-        for (final File fileEntry : folder.listFiles()) {
-            if (!fileEntry.isDirectory()) {
-                Counter counter = countersFactory.getCounter(getFolderPath() + fileEntry.getName());
-                wordsContainers.add(counter.parse());
-            }
-        }
-        return wordsContainers;
-    }
+	public void setFolderPath(String folderPath)
+	{
+		this.folderPath = folderPath;
+	}
+
+	ArrayList<WordsContainer> start() throws IOException
+	{
+		File folder = new File(this.folderPath);
+		ArrayList<WordsContainer> wordsContainers = new ArrayList<>();
+		for (final File fileEntry : folder.listFiles())
+		{
+			if (!fileEntry.isDirectory())
+			{
+				Counter counter = countersFactory.getCounter(getFolderPath() + fileEntry.getName());
+				wordsContainers.add(counter.parse());
+			}
+		}
+		return wordsContainers;
+	}
+
+	public void setCountersFactory(final CountersFactory countersFactory)
+	{
+		this.countersFactory = countersFactory;
+	}
 }
